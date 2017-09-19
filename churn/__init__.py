d65cedbd8eb7ccaf716139f148c7d08892f0dc2f
Saurav Raj <sauravraj009@gmail.com>
2017-09-20 01:37:03 +0530

    Make the config file to have different profiles like dev, testing, prod

--- churn/__init__.py
+++ churn/__init__.py
@@ -1,11 +1,13 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
 # application.config.from_object(os.environ['APP_SETTINGS'])
+application.config.from_object(os.environ['APP_SETTINGS'])
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # application.secret_key = 'my-secrest-key'
-application.config.from_pyfile('config.py')
+# application.config.from_pyfile('config.py')
 db = SQLAlchemy(application)
 
 from churn import views, models
