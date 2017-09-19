5eccd214c89a3f23cdc7183a249fde68408f6340
Saurav Raj <sauravraj009@gmail.com>
2017-09-19 23:39:11 +0530

    Start of an ML Web Project

---
+++ application.py
@@ -0,0 +1,5 @@
+#!flask/bin/python
+from churn import app as application
+
+if __name__ == '__main__':
+    application.run()
---
+++ churn/config.py
@@ -0,0 +1,9 @@
+import os
+
+# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+SQLALCHEMY_POOL_RECYCLE = 3600
+
+WTF_CSRF_ENABLED = True
+
+SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
